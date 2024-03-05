1ec3c8cfa900b495aef9a5ca1fbcbb7f576851e1
Ucsus <ucsus@exbo.ru>
2024-03-06 00:47:03 +0300

    fix: remove unnecessary code

--- src/Games/BrainCalc.php
+++ src/Games/BrainCalc.php
@@ -8,9 +8,8 @@ const GAME_DESCRIPTION = 'What is the result of the expression?';
 function handler(): void
 {
     $getQuestionWithAnswer = function (): array {
-        $numsArray = range(1, 30);
         $operatorsArray = ['+', '-', '*'];
-        $randomNumsArray = [$numsArray[array_rand($numsArray)], $numsArray[array_rand($numsArray)]];
+        $randomNumsArray = [rand(1, 30), rand(1, 30)];
         $randomNum1 = max($randomNumsArray);
         $randomNum2 = min($randomNumsArray);
         $randomOperator = $operatorsArray[array_rand($operatorsArray)];
