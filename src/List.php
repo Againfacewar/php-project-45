09ba075be9cdff1c25410a190d2e34dd281f5e54
Ucsus <ucsus@exbo.ru>
2024-03-07 23:56:59 +0300

    hot fix

--- src/List.php
+++ src/List.php
@@ -6,18 +6,17 @@ use cli\Table;
 
 function getGamesList(): void
 {
-//    $binDirectory = __DIR__ . '/../bin';
-//
-//    if (is_dir($binDirectory)) {
-//        $directories = scandir($binDirectory);
-//        $notAllowDirectories = ['..', '.', 'game-list'];
-//        var_dump($directories);
-//        var_dump($notAllowDirectories);
-//        $mappedDirectories = array_map(function ($value) {
-//            return "make $value";
-//        }, array_diff($directories, $notAllowDirectories));
-//        $table = new Table();
-//        $table->setHeaders($mappedDirectories);
-//        $table->display();
-//    }
+    $binDirectory = __DIR__ . '/../bin';
+
+    if (is_dir($binDirectory)) {
+        $directories = scandir($binDirectory);
+        $notAllowDirectories = ['..', '.', 'game-list'];
+        $diff = array_diff($directories, $notAllowDirectories);
+        $mappedDirectories = array_map(function ($value) {
+            return "make $value";
+        }, $diff);
+        $table = new Table();
+        $table->setHeaders($mappedDirectories);
+        $table->display();
+    }
 }
